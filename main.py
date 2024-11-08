66c0b5ba33bd959c2eab98f4d0d79b2aae7c88f4
Andrey <andreyfabris.a@gmail.com>
2024-11-08 19:06:52 -0300

    corrigido valores do PLc

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import Calibration
 
 
 
-'''Run calibration file to plot the RSSI curve and find the Path-loss coefficent to each anchor'''
-Calibration.RSSI_model()
+'''
+Run calibration to plot the RSSI curve and find the Path-loss coefficent to each anchor
+Plot graphics: use the config.yaml to determine whether to display no plots, only the first plot, or all RSSI models.
+The value of Path-loss coefficent has to be manually assigned in confing.yaml
+'''
+Calibration.RSSI_model() 

9e4d0e76e4746d38e142598a52c57bf22b5b001c
Andrey <andreyfabris.a@gmail.com>
2024-11-08 23:00:49 -0300

    comentado calibracao no main.oy

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Run calibration to plot the RSSI curve and find the Path-loss coefficent to each
 Plot graphics: use the config.yaml to determine whether to display no plots, only the first plot, or all RSSI models.
 The value of Path-loss coefficent has to be manually assigned in confing.yaml
 '''
-Calibration.RSSI_model() 
+# Calibration.RSSI_model() 
 
 
 Movement.run(case=1)

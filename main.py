175b244a54fe4c27ab5446e5962f0961dc17b092
Andrey <andreyfabris.a@gmail.com>
2024-11-10 12:53:07 -0300

    ajustado comentarios no main e config.yaml

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import numpy as np
 import Calibration
 import Movement
 
-
+'''
+IMPORTANT: The dataset file path needs to be specified correctly in the config.yaml file.
+'''
 
 '''
-Run calibration to plot the RSSI curve and find the Path-loss coefficent to each anchor
-Plot graphics: use the config.yaml to determine whether to display no plots, only the first plot, or all RSSI models.
-The value of Path-loss coefficent has to be manually assigned in confing.yaml
+Run calibration to plot the RSSI curve and view the path-loss coefficient for each anchor.
+
+Plot settings: Use the config.yaml file to determine whether to display no plots, only the first anchor plot, or all RSSI models.
+
+The path-loss coefficient value must be manually assigned in config.yaml.
 '''
 Calibration.RSSI_model() 
 
 '''
-Run movements to plot ans view the results for the estimate position given by the IPS methods.
-Plot graphics: use the config.yaml to determine whether to display the plots for predicted path, average error and CDF
+Run movements to plot and view the results for the estimated position provided by the IPS methods.
+
+Plot settings: Use the config.yaml file to determine whether to display plots for the predicted path, average error, and CDF.
 '''
 
 Movement.run(case=1)
